44bcce4d2df93f4c9cd6540078f9d8e046fd5c23
Rachel Starr <starr.raylin@gmail.com>
2019-12-09 10:09:37 -0500

    adds code to render review cards on home page

--- public/js/homeRender.js
+++ public/js/homeRender.js
@@ -1,13 +1,40 @@
+async function getReview(reviewId) {
+    const reply = await axios({
+        method: 'get',
+        url: `/api/reviews/${reviewId}`
+    });
+    return reply;
+}
+
+async function renderReviewCard(review) {
+    let reviewElement = $(`<div id="${review.id}" class="card"></div>`);
+    reviewElement.append($(`<p>${review.user_id}</p>`));
+    reviewElement.append($(`<p>${review.game_id}</p>`));
+    reviewElement.append($(`<p>${review.body}</p>`));
+    reviewElement.append($(`<p>${review.rating}</p>`));
+    reviewElement.append($(`<p>${review.date_created}</p>`));
+    reviewElement.append($(`<p>${review.date_updated}</p>`));
+    $("#feedContent").append(reviewElement);
+}
+
+async function getReviewFeed() {
+    const reply = await axios({
+        method: 'get',
+        url: "http://localhost:3000/reviews/"
+    });
+    return reply;
+}
 
 
 
+async function renderReviewFeed() {
+    const reviews = await getReviewFeed();
+    for(let i = 0; i < reviews.length; i++) {
+        renderReviewCard(reviews[i]);
+    }
+}
 
 
-
-$(function () {
-
-    // $("#login").on("click", null, login);
-    // $("#createaccount").on("click", null, makeaccount);
-
-    
+$(document).ready(() => {
+    renderReviewFeed();
 });

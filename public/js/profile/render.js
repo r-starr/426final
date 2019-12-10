a67cf6843786579c136b0962069d8725e4a9d4da
katkm <martin.katherine.k@gmail.com>
2019-12-10 11:31:40 -0500

    other

--- public/js/profile/render.js
+++ public/js/profile/render.js
@@ -1,13 +1,29 @@
 
 function renderReviewCard(review) {
-    let reviewElement = $(`<div id="${review.id}" class="card"></div>`);
-    reviewElement.append($(`<p>${review.user_id}</p>`));
-    reviewElement.append($(`<p>${review.game_id}</p>`));
-    reviewElement.append($(`<p>${review.body}</p>`));
-    reviewElement.append($(`<p>${review.rating}</p>`));
-    reviewElement.append($(`<p>${review.date_created}</p>`));
-    reviewElement.append($(`<p>${review.date_updated}</p>`));
-    $("#feedContent").append(reviewElement);
+    let object = `
+    <div class="card">
+        <div class="card-content">
+            <div class="media">
+                <div class="media-content">
+                    <p class="title is-4 has-text-info	">AUTHOR REVIEWED THIS GAME</p>
+                </div>
+            </div>
+        
+            <div class="content" id = "words">
+                    <p>REVIEW BODY</p>
+                    <br />
+            </div>
+        </div>
+    </div>
+    `;
+    // let reviewElement = $(`<div id="${review.id}" class="card"></div>`);
+    // reviewElement.append($(`<p>${review.user_id}</p>`));
+    // reviewElement.append($(`<p>${review.game_id}</p>`));
+    // reviewElement.append($(`<p>${review.body}</p>`));
+    // reviewElement.append($(`<p>${review.rating}</p>`));
+    // reviewElement.append($(`<p>${review.date_created}</p>`));
+    // reviewElement.append($(`<p>${review.date_updated}</p>`));
+    $("#feedContent").append($(object));
 }
 
 async function renderReviewFeed() {
@@ -18,14 +34,15 @@ async function renderReviewFeed() {
 }
 
 function renderGameCard(game) {
-    let gameElement = $(`<div id="${game.id}" class="card"></div>`);
-    gameElement.append($(`<p>${game.id}</p>`));
-    gameElement.append($(`<p>${game.name}</p>`));
-    gameElement.append($(`<p>${game.developer}</p>`));
-    gameElement.append($(`<p>${game.platform}</p>`));
-    gameElement.append($(`<p>${game.device}</p>`));
-    gameElement.append($(`<p>${game.releaseYear}</p>`));
-    $("#feedContent").append(gameElement);
+
+    // let gameElement = $(`<div id="${game.id}" class="card"></div>`);
+    // gameElement.append($(`<p>${game.id}</p>`));
+    // gameElement.append($(`<p>${game.name}</p>`));
+    // gameElement.append($(`<p>${game.developer}</p>`));
+    // gameElement.append($(`<p>${game.platform}</p>`));
+    // gameElement.append($(`<p>${game.device}</p>`));
+    // gameElement.append($(`<p>${game.releaseYear}</p>`));
+    $("#feedContent").append($(object));
 }
 
 function generateGameCard(game) {
@@ -48,4 +65,5 @@ function generateCardFooter() {
 }
 
 $(document).ready(() => {
+    renderReviewFeed();
 });
